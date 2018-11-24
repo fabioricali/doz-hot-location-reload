7875bd4397254b786b66c7baff7ed56b1412297b
Fabio Ricali <ricalifabio@gmail.com>
2018-11-24 23:03:08 +0100

    improvement

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 module.exports = function (_module) {
     if (_module.hot) {
-        _module.hot.accept(function () {
-            location.reload();
+        _module.hot.dispose(function () {
+            location.reload(true);
+            throw new Error('Waiting reload... please do not consider this error :)');
         });
     }
 };
